b196c05dcaac76bd31642280b98a29f8f4875545
vanisa <anggrainivanisayunia@gmail.COM>
2025-04-09 16:38:30 +0700

    Memberi pilihan kepada pengguna

--- app/src/main/java/com/vanisa0066/moody/ui/screen/MainScreen.kt
+++ app/src/main/java/com/vanisa0066/moody/ui/screen/MainScreen.kt
@@ -1,15 +1,24 @@
 package com.vanisa0066.moody.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,8 +28,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,9 +64,19 @@ fun ScreenContent(modifier: Modifier = Modifier){
     var tambahCatatan by rememberSaveable { mutableStateOf("") }
     var persenanMood by rememberSaveable { mutableStateOf("") }
 
+    val radioOptions = listOf(
+        stringResource(id= R.string.sedih),
+        stringResource(id= R.string.netral),
+        stringResource(id= R.string.senang)
+    )
+    var mood by rememberSaveable { mutableStateOf(radioOptions[0]) }
+
     Column (
-        modifier = modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
             text = stringResource(id = R.string.intro_moody),
@@ -84,8 +106,53 @@ fun ScreenContent(modifier: Modifier = Modifier){
             ),
             modifier = Modifier.fillMaxWidth()
         )
+       Row (
+           modifier = Modifier
+               .padding(top = 6.dp)
+               .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+       ){
+           radioOptions.forEach{ text ->
+               MoodOption(
+                   label = text,
+                   isSelected = mood == text,
+                   modifier = Modifier
+                       .selectable(
+                           selected = mood == text,
+                           onClick = { mood = text},
+                           role = Role.RadioButton
+                       )
+                       .weight(1f)
+                       .padding(16.dp)
+               )
+           }
+           Button(
+                onClick = {},
+               modifier = Modifier.padding(top = 8.dp),
+               contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+           ) {
+               Text(text = stringResource(R.string.cek_kegiatan))
+           }
+       }
+        }
     }
-}
+    @Composable
+    fun MoodOption (label: String, isSelected: Boolean, modifier: Modifier) {
+        Row (
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            RadioButton(selected = isSelected, onClick = null)
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+    }
+
+
+
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
