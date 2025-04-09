532ee7a81f92a6b3b89bc8d370eba6ca8b3eeb4b
vanisa <anggrainivanisayunia@gmail.COM>
2025-04-09 15:43:33 +0700

    meminta input data

--- app/src/main/java/com/vanisa0066/moody/ui/screen/MainScreen.kt
+++ app/src/main/java/com/vanisa0066/moody/ui/screen/MainScreen.kt
@@ -1,17 +1,30 @@
 package com.vanisa0066.moody.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vani0066.moody.R
 import com.vani0066.moody.ui.theme.MoodyTheme
 
@@ -36,10 +49,42 @@ fun MainScreen(){
 }
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello android",
-        modifier = modifier
-    )
+    var tambahCatatan by rememberSaveable { mutableStateOf("") }
+    var persenanMood by rememberSaveable { mutableStateOf("") }
+
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        Text(
+            text = stringResource(id = R.string.intro_moody),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = tambahCatatan,
+            onValueChange = { tambahCatatan = it},
+            label = { Text(text = stringResource((R.string.tambah_catatan))) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = persenanMood,
+            onValueChange = { persenanMood = it },
+            label = { Text(text = stringResource(R.string.persenan_mood)) },
+            trailingIcon = { Text(text = "%") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
