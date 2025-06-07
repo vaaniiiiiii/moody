590fb6a986f362b7d9e0947634fcc3c42d281092
vanisa <anggrainivanisayunia@gmail.COM>
2025-06-07 17:33:05 +0700

    Membuat indikator loading image

--- app/src/main/java/moody/ui/screen/GambarScreen.kt
+++ app/src/main/java/moody/ui/screen/GambarScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -133,6 +134,7 @@ fun ListItem(gambar: Gambar){
                 .build(),
             contentDescription = stringResource(R.string.gambar_daily, gambar.judul),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column (
