e2b3b6d9a6d57ca1a6c0357756a22c4d6557b724
vanisa <anggrainivanisayunia@gmail.COM>
2025-06-13 13:45:27 +0700

    menambahkan radio button untuk inputan

--- app/src/main/java/moody/ui/screen/DailyDialog.kt
+++ app/src/main/java/moody/ui/screen/DailyDialog.kt
@@ -2,93 +2,147 @@ package moody.ui.screen
 
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import coil.compose.rememberAsyncImagePainter
 import com.vani0066.moody.R
 import moody.ui.theme.MoodyTheme
 
 @Composable
 fun DailyDialog(
     bitmap: Bitmap?,
+    imageUrl: String? = null,
+    titleInitial: String = "",
+    moodInitial: String = "",
     onDismissRequest: () -> Unit,
     onConfirmation: (String, String) -> Unit
-){
-    var title by remember { mutableStateOf("") }
-    var mood by remember { mutableStateOf("") }
+) {
+    var title by rememberSaveable { mutableStateOf(titleInitial) }
+    var mood by rememberSaveable { mutableStateOf(moodInitial) }
 
-    Dialog(onDismissRequest = {onDismissRequest()}) {
+
+    val radioOptions = listOf(
+        stringResource(id = R.string.senang),
+        stringResource(id = R.string.sedih)
+    )
+
+
+
+    Dialog(onDismissRequest = { onDismissRequest()}) {
         Card(
             modifier = Modifier.padding(16.dp),
-            shape = RoundedCornerShape(16.dp),
+            shape = RoundedCornerShape(16.dp)
         ) {
-            Column (
+            Column(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-            ){
-                Image(
-                    bitmap = bitmap!!.asImageBitmap(),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth().aspectRatio(1f)
-                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                ) {
+                    if (bitmap != null) {
+                        Log.d("DailyPhoto", "Menampilkan bitmap dari kamera")
+                        Image(
+                            bitmap = bitmap.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                        )
+                    }
+                    else if (!imageUrl.isNullOrEmpty()) {
+                        Log.d("DailyPhoto", "Menampilkan gambar dari url")
+                        Image(
+                            painter = rememberAsyncImagePainter(imageUrl),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                        )
+                    }
+                }
                 OutlinedTextField(
                     value = title,
-                    onValueChange = {title = it},
-                    label = { Text(text = stringResource(id = R.string.judul)) },
-                    maxLines = 1,
+                    onValueChange = { title = it },
+                    label = { Text(text = stringResource(id = R.string.title)) },
                     keyboardOptions = KeyboardOptions(
                         capitalization = KeyboardCapitalization.Words,
                         imeAction = ImeAction.Next
                     ),
                     modifier = Modifier.padding(top = 8.dp)
                 )
-                OutlinedTextField(
-                    value = mood,
-                    onValueChange = {mood = it},
-                    label = { Text(text = stringResource(id = R.string.Hari)) },
-                    maxLines = 1,
-                    keyboardOptions = KeyboardOptions(
-                        capitalization = KeyboardCapitalization.Words,
-                        imeAction = ImeAction.Next
-                    ),
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-                Row (
-                    modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 6.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+                        .padding(8.dp)
+                ) {
+                    radioOptions.forEach { text ->
+                        HabitatOption(
+                            label = text,
+                            isSelected = mood == text,
+                            modifier = Modifier
+                                .selectable(
+                                    selected = mood == text,
+                                    onClick = { mood = text },
+                                    role = Role.RadioButton
+                                )
+                                .weight(1f)
+                                .padding(4.dp)
+                        )
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
                     horizontalArrangement = Arrangement.Center
-                ){
+                ) {
                     OutlinedButton(
-                        onClick = {onDismissRequest()},
+                        onClick = {onDismissRequest() },
                         modifier = Modifier.padding(8.dp)
                     ) {
                         Text(text = stringResource(R.string.tombol_batal))
                     }
                     OutlinedButton(
-                        onClick = {onConfirmation(title, mood)},
+                        onClick = {
+                            onConfirmation(title, mood)
+                        },
                         enabled = title.isNotEmpty() && mood.isNotEmpty(),
                         modifier = Modifier.padding(8.dp)
                     ) {
@@ -100,15 +154,43 @@ fun DailyDialog(
     }
 }
 
+@Composable
+fun HabitatOption(
+    label: String,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .padding(vertical = 8.dp, horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        RadioButton(
+            selected = isSelected,
+            onClick = null
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun AddDialogPreview(){
-    MoodyTheme {
-        DailyDialog(
+fun AddDialogPreview() {
+    MoodyTheme  {
+        DailyDialog (
             bitmap = null,
+            titleInitial = "rapat",
+            moodInitial = "senang",
             onDismissRequest = {},
-            onConfirmation = { _, _,-> }
+            onConfirmation = {_, _->}
         )
     }
 }
